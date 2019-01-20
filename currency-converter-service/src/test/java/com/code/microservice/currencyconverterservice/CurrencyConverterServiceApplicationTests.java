fada408c9f4c73d55b46535161f4d476470ac4a3
Nagenderr2Code <nagender2u@yahoo.co.in>
2019-01-20 14:50:50 -0500

    currency-converter-service initial Commit Closes #6

---
+++ currency-converter-service/src/test/java/com/code/microservice/currencyconverterservice/CurrencyConverterServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.code.microservice.currencyconverterservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyConverterServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

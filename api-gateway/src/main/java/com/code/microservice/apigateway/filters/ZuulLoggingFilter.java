ec227f60a040b6eff9057b452fb1c77fca72972f
Nagenderr2Code <nagender2u@yahoo.co.in>
2019-01-21 13:32:58 -0500

    Enable API gateway Closes #14

---
+++ api-gateway/src/main/java/com/code/microservice/apigateway/filters/ZuulLoggingFilter.java
@@ -0,0 +1,38 @@
+package com.code.microservice.apigateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    Logger log= LoggerFactory.getLogger(getClass());
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("request->{}" + request);
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+}
--- currency-converter-service/src/main/java/com/code/microservice/currencyconverterservice/client/CurrencyConverterClient.java
+++ currency-converter-service/src/main/java/com/code/microservice/currencyconverterservice/client/CurrencyConverterClient.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange")
+@FeignClient(name = "api-gateway")
 @RibbonClient
 public interface CurrencyConverterClient {
 
-    @GetMapping("/exchange/{from}/to/{to}")
+    @GetMapping("/currency-exchange/exchange/{from}/to/{to}")
     public CurrencyConverted convert(@PathVariable String from, @PathVariable String to);
 
     }
